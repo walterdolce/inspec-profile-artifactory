70b1abe2f84c0aa4728f4ed08d4ddc38500e3e84
Walter Dolce <walterdolce@gmail.com>
2018-06-11 14:52:44 +0100

    Ensure JAVA_HOME is set within the SUT

--- controls/java.rb
+++ controls/java.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-control 'java-1.0' do
+control 'java-installation-1.0' do
   impact 1.0
   title 'Verify Java is installed'
   desc "Verify Java is installed. If not, Artifactory can't be installed."
@@ -11,5 +11,15 @@ control 'java-1.0' do
 
   describe command('java -version') do
     its('stderr') { should contain "1.8" }
+  end
+end
+
+control 'java-os-environment-setup-1.0' do
+  impact 0.7
+  title 'Verify Java environment variables are setup'
+  desc 'Verify Java environment variables are setup'
+
+  describe os_env('JAVA_HOME') do
+    its('content') { should_not eq "" }
   end
 end
